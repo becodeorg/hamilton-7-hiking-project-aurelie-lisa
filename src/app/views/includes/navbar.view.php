7d0264dfda5e8cdeb6c1f5d18287dff74c5d9704
Avanhellemont <vanhellemont.aurelie@gmail.com>
2022-12-21 09:12:05 +0100

    commit changes 21-12 09:11

---
+++ src/app/views/includes/navbar.view.php
@@ -0,0 +1,13 @@
+<nav>
+    <ul>
+        <a href="/"><li>Home</li></a>
+        <?php if ($_SESSION['user']['loggedIn']) { ?>
+            <a href="/logout"><li>Logout</li></a>
+        <?php } else { ?>
+            <a href="/login"><li>Login</li></a>
+            <a href="/registration"><li>Registration</li></a>
+        <?php } ?>
+            <a href="/allhike"><li>Hikes</li></a>
+    </ul>
+</nav>
+
