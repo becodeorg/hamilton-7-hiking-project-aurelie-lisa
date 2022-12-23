d6fcd8b5a2269a006efaae40a51d37cf4238e773
lilouMazzarisi <lisa_mazzarisi@hotmail.com>
2022-12-23 09:07:56 +0100

    update 09:07 23.12

--- src/app/views/profile.view.php
+++ src/app/views/profile.view.php
@@ -6,7 +6,7 @@
     <p>Email: <?= $_SESSION['user']['email'];?></p>
     <h3> My Hikes </h3>
     <button class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-        <a href="/allhike" class="text-gray-900 dark:text-white">Add Hikes</a>
+        <a href="/addhike" class="text-gray-900 dark:text-white">Add Hikes</a>
     </button>
     <?= var_dump($_SESSION['user']['id']); ?>
     <?php foreach ($hikes as $hike) : ?>
