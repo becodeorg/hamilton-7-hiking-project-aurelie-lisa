9a22175e73e6401d3511247de621bb68bf9493b2
Avanhellemont <vanhellemont.aurelie@gmail.com>
2022-12-23 15:30:34 +0100

    update 23-12-22 15:30

--- src/app/views/includes/footer.view.php
+++ src/app/views/includes/footer.view.php
@@ -20,7 +20,7 @@
             </ul>
         </div>
         <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-        <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© Becode 2022 <a href="https://flowbite.com/" class="hover:underline">Flowbite™</a>. All Rights Reserved.
+        <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© Becode 2022 <a href="/" class="hover:underline">Hiking Adventure™</a>. All Rights Reserved.
     </span>
 </footer>
         </div>
