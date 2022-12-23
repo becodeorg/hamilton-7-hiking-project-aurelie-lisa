ff3e5097c0dcd1f605f5de3cd307b2579cec7b5e
Avanhellemont <vanhellemont.aurelie@gmail.com>
2022-12-23 09:10:19 +0100

    update 23-12-22 09:10

--- src/app/views/includes/footer.view.php
+++ src/app/views/includes/footer.view.php
@@ -1,18 +1,34 @@
-<footer>
+<footer class="p-4 bg-white rounded-lg shadow md:px-6 md:py-8 dark:bg-gray-900">
     <section>
-        <div class="copyright">
-            &copy; BeCode <?php echo date('Y')?>
+        <div class="sm:flex sm:items-center sm:justify-between">
+            <a href="/">
+                <img src="images/Hiking_Logo.png" class="h-20 flex items-center" alt="hiking Adventure Logo" />
+            </a>
+            <ul class="flex flex-wrap items-center mb-6 text-sm text-gray-500 sm:mb-0 dark:text-gray-400">
+                <li>
+                    <a href="#" class="mr-4 hover:underline md:mr-6 ">About</a>
+                </li>
+                <li>
+                    <a href="#" class="mr-4 hover:underline md:mr-6">Privacy Policy</a>
+                </li>
+                <li>
+                    <a href="#" class="mr-4 hover:underline md:mr-6 ">Licensing</a>
+                </li>
+                <li>
+                    <a href="#" class="hover:underline">Contact</a>
+                </li>
+            </ul>
         </div>
-        <div>
-            <div id="icons">
-                <a href="http://www.twitter.fr"></a>
-                <a href="http://www.facebook.fr"></a>
-                <a href="http://www.instagram.com"></i></a
-                >
-            </div>
+        <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© Becode 2022 <a href="https://flowbite.com/" class="hover:underline">Flowbite™</a>. All Rights Reserved.
+    </span>
+</footer>
         </div>
     </section>
 </footer>
 </body>
 </html>
+
+
+
 
